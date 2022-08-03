25e9aa3f3d1103371bfa6728e0bf3fcf473054ba
phanduykhanh1009 <phanduykhanh1009@gmail.com>
2022-08-03 23:10:12 +0700

    test SDK

---
+++ PrintDemo/Source/PrintService.swift
@@ -0,0 +1,15 @@
+//
+//  PrintService.swift
+//  PrintDemo
+//
+//  Created by KhanhPD2.APL on 03/08/2022.
+//
+
+import Foundation
+
+open class PrintService {
+    public static let shared = PrintService()
+    public func printDemo() {
+        print("KhanhPD2 Hello")
+    }
+}
